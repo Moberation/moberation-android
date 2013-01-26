50c65b1144c958645635630f8131b5c85681ab66
Jaran Nilsen <jaran.nilsen@gmail.com>
2013-01-26 10:59:00 +0100

    added request dispatching to server for starting a new game. needs additional functionality once server variables and response has been determined.

---
+++ src/com/moberation/android/AppConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.moberation.android;
+
+/**
+ * Contains configuration constants
+ * 
+ * @author jaran
+ * 
+ */
+public final class AppConfig {
+
+	public static String SERVER_BASE_URL = "http://localhost:8080/moberation/";
+}
--- src/com/moberation/android/StartNewGameActivity.java
+++ src/com/moberation/android/StartNewGameActivity.java
@@ -1,15 +1,32 @@
 package com.moberation.android;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.moberation.android.utils.AbstractTextWatcher;
 
 public class StartNewGameActivity extends Activity {
+
+	private static final String TAG = StartNewGameActivity.class
+			.getSimpleName();
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -17,8 +34,74 @@ public class StartNewGameActivity extends Activity {
 		setContentView(R.layout.activity_start_new_game);
 
 		final Button startButton = (Button) findViewById(R.id.buttonStartNewGame);
+		final EditText gameName = (EditText) findViewById(R.id.editNewGameName);
+		final Spinner roleSpinner = (Spinner) findViewById(R.id.spinnerRole);
+		final Activity thisActivity = this;
+		startButton.setOnClickListener(new View.OnClickListener() {
 
-		EditText gameName = (EditText) findViewById(R.id.editNewGameName);
+			@Override
+			public void onClick(final View v) {
+
+				v.setEnabled(false);
+
+				HttpParams httpParameters = new BasicHttpParams();
+				int timeoutConnection = 3000;
+				HttpConnectionParams.setConnectionTimeout(httpParameters,
+						timeoutConnection);
+				int timeoutSocket = 5000;
+				HttpConnectionParams
+						.setSoTimeout(httpParameters, timeoutSocket);
+				HttpClient client = new DefaultHttpClient(httpParameters);
+
+				try {
+					String requestUrl = AppConfig.SERVER_BASE_URL
+							+ "/NewGame?name=" + gameName.getText() + "&role="
+							+ roleSpinner.getSelectedItem();
+
+					Log.d(TAG, "Requesting URL: " + requestUrl);
+
+					HttpGet get = new HttpGet(requestUrl);
+					HttpResponse response = client.execute(get);
+					if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+
+						// New game started!
+
+					} else {
+
+						throw new IllegalStateException(
+								"Server returned status: "
+										+ response.getStatusLine().toString());
+					}
+
+				} catch (Exception e) {
+
+					Log.e(TAG, "An error occured while requesting new game:"
+							+ e.getMessage());
+
+					AlertDialog.Builder builder = new AlertDialog.Builder(
+							thisActivity);
+					builder.setMessage(R.string.newgame_servererror_text)
+							.setTitle(R.string.newgame_servererror_title);
+					builder.setPositiveButton(R.string.ok,
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(
+										final DialogInterface dialog,
+										final int which) {
+
+									// OK was clicked
+
+								}
+							});
+
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				}
+				v.setEnabled(true);
+			}
+		});
+
 		gameName.addTextChangedListener(new AbstractTextWatcher() {
 
 			@Override
